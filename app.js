639a226cf6e7e0a2df7cc4b72a0051143efd37b7
Victor Rocha <victor.barboza.15@gmail.com>
2019-05-15 19:09:05 -0400

    landing page working

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var landingPageRouter = require('./routes/landingPage')
 
 var app = express();
 
@@ -19,7 +20,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', landingPageRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
