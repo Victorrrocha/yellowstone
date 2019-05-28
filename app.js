9c95971644636da87865a9a720fae6c8d7e34488
Victor Barbosa Rocha <victor.barbosa.18@outlook.com>
2019-05-28 15:00:31 -0400

    autentification

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
 var createError = require('http-errors');
-var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var landingPageRouter = require('./routes/landingPage')
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+//passport config
+require('./config/passport')(passport);
+
+//DB CONFIG
+const db = require('./config/keys').MongoURI;
+
+//CONNECT TO MONGO
+mongoose.connect(db, {useNewUrlParser: true})
+.then(()=> console.log('Mongodb Connected'))
+.catch(err=> console.log(err));
+
+// EJS
+app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+//Bodyparser
+app.use(express.urlencoded({extended: true}));
+
+//Express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+//Global var
+app.use((req, res, next) =>{
+  res.locals.success_msg = req.flash('sucess_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//ROUTES
+var indexRouter = require('./routes/index.js');
+var usersRouter = require('./routes/users.js');
+var landingPageRouter = require('./routes/landingPage.js')
 
 app.use('/', landingPageRouter);
 app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://victor:1234@yellowstone-kmrzw.mongodb.net/test?retryWrites=true'
+}
